79c212acfbb9ee85dee77b31971341f3333bb7a3
phpbae <z1004man@naver.com>
2018-08-07 14:08:05 +0900

    promise 예제 작성
    모르겠다..ㅋㅋㅋㅋㅋㅋ

--- src/promise_example/promise_example.js
+++ src/promise_example/promise_example.js
@@ -7,7 +7,7 @@ const promiseExample = () => {
 
     console.log('promise 예제 start--------------------------------------------------');
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let sumResult = 0;
+    let sumResult2 = 0;
 
     function addNumber(a, b) {
         return new Promise((resolve => {
@@ -15,24 +15,28 @@ const promiseExample = () => {
         }));
     }
 
-    // function addNumber(a, b) {
-    //     return new Promise(function (resolve, reject) {
-    //         resolve(a + b)
-    //     });
-    // }
+    // Promise를 사용안하고 reduce() 를 이용하는 방법도 있다.
+    const sumResult3 = numbers.reduce((a, b) => a + b, 0);
+    console.log('sumResult3 : ' + sumResult3);
 
 
+    //해결2
+    function sumChain(prevSum) {
+        let nextNum = numbers.shift();
+        sumResult2 = prevSum;
 
-    numbers.forEach((value, index) => {
+        if (nextNum) {
+            return addNumber(sumResult2, nextNum)
+                .then(sumChain);
+        } else {
+            return sumResult2;
+        }
+    }
 
-        addNumber(value, sumResult)
-            .then(function (resolve) {
-                //sumResult = resolve;
-
-            });
-
+    sumChain(sumResult2).then((resolve) => {
+        console.log('sumResult2 : ' + resolve); //55
     });
-    console.log(sumResult);
+
     console.log('promise 예제 end--------------------------------------------------');
 
 }
